28e1931eadecb3d14186e1d76a64529313494ec5
Shwetansh Srivastava <shwetanshs@adobe.com>
2024-12-17 18:05:51 +0530

    Add: success API call

--- app/src/main/java/com/example/gw_ajo_b2b/networking/Networking.kt
+++ app/src/main/java/com/example/gw_ajo_b2b/networking/Networking.kt
@@ -71,64 +71,42 @@ fun makeGraphQLRequest(
 
 
 fun makeAPICall() {
-    val getAllBuyingGroupsV2Query = """
-    query getAllBuyingGroupsV2(
-        "${'$'}search: String,
-        "${'$'}sortBy: String,
-        "${'$'}order: String,
-        "${'$'}filter: String,
-        "${'$'}next: String,
-        "${'$'}maxReturn: Int
-    ) {
-        buyingGroupsV2(
-            search: "${'$'}search",
-            sortBy: "${'$'}sortBy",
-            order: "${'$'}order",
-            filter: "${'$'}filter",
-            next: "${'$'}next",
-            maxReturn: "${'$'}maxReturn"
-        ) {
-            result {
-                id
-                name
-                status
-                buyingGroupTemplate {
-                    id
-                    name
-                    __typename
-                }
-                account {
-                    id
-                    name
-                    __typename
-                }
-                solutionInterest {
-                    id
-                    name
-                    __typename
-                }
-                completenessScore
-                engagementScore
-                createdAt
-                updatedAt
-                __typename
-            }
-            totalItems
-            next
-            errors {
-                code
-                message
-                __typename
-            }
-            __typename
-        }
-    }
-    """.trimIndent()
 
+    val queryPayload = """
+    {
+      "operationName": "getAllBuyingGroupsV2",
+      "variables": {
+        "filter": "",
+        "maxReturn": 50,
+        "next": "",
+        "order": "desc",
+        "search": "",
+        "sortBy": "id"
+      },
+      "query": "query getAllBuyingGroupsV2(${'$'}search: String, ${'$'}sortBy: String, ${'$'}order: String, ${'$'}filter: String, ${'$'}next: String, ${'$'}maxReturn: Int) {\n  buyingGroupsV2(\n    search: ${'$'}search\n    sortBy: ${'$'}sortBy\n    order: ${'$'}order\n    filter: ${'$'}filter\n    next: ${'$'}next\n    maxReturn: ${'$'}maxReturn\n  ) {\n    result {\n      id\n      name\n      status\n      buyingGroupTemplate {\n        id\n        name\n        __typename\n      }\n      account {\n        id\n        name\n        __typename\n      }\n      solutionInterest {\n        id\n        name\n        __typename\n      }\n      completenessScore\n      engagementScore\n      createdAt\n      updatedAt\n      __typename\n    }\n    totalItems\n    next\n    errors {\n      code\n      message\n      __typename\n    }\n    __typename\n  }\n}",
+      "extensions": {
+        "accessToken": "821834e5-62ef-4eb8-a1a9-37e0e1ed6248:st:842-STS-238",
+        "dataCenter": "st",
+        "munchkinId": "842-STS-238",
+        "clientContext": {
+          "applicationId": "sapphireBuyingGroups:stage20241206180737",
+          "deviceId": "8a3f9603-bc60-11ef-991d-b9c4e0735b3e",
+          "groupId": "22DE1AF9643D97B30A494133@AdobeOrg",
+          "historyId": "9b6f2710-bc60-11ef-991d-b9c4e0735b3e",
+          "instanceId": "8a3f9602-bc60-11ef-991d-b9c4e0735b3e",
+          "sessionId": "https://ims-na1-stg1.adobelogin.com/ims/session/v1/ZWE2ZWMwODMtZGEyNC00Nzc1LWI0ZTMtZThkNTQzZjA3OTEzLS0wNDNFMTk5QzY2QjJDOEU2MEE0OTQwMUJAMjk3YTA2Y2E1ZTk3NmU4ZDBhNDk0MjA0",
+          "windowId": "9b6f2711-bc60-11ef-991d-b9c4e0735b3e"
+        }
+      },
+      "sandboxName": "prod",
+      "sandboxType": "production",
+      "isDefaultSandbox": "true"
+    }
+""".trimIndent()
 
     val token = "Bearer eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEtc3RnMS1rZXktYXQtMS5jZXIiLCJraWQiOiJpbXNfbmExLXN0ZzEta2V5LWF0LTEiLCJpdHQiOiJhdCJ9.eyJpZCI6IjE3MzQ0MzA4MzA1MzNfYjRiY2UzMjYtMjRlNC00OGE5LTlmNGItYzc4MmI0ODcwNWE2X3V3MiIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiJleGNfYXBwIiwidXNlcl9pZCI6IjE2MjYxOTgwNjc1MzUwNEEwQTQ5NDIxNUAzZDUzMWFkYTY0M2RiZmIwNDk0MTI4LmUiLCJzdGF0ZSI6IntcInNlc3Npb25cIjpcImh0dHBzOi8vaW1zLW5hMS1zdGcxLmFkb2JlbG9naW4uY29tL2ltcy9zZXNzaW9uL3YxL1pXRTJaV013T0RNdFpHRXlOQzAwTnpjMUxXSTBaVE10WlRoa05UUXpaakEzT1RFekxTMHdORE5GTVRrNVF6WTJRakpET0VVMk1FRTBPVFF3TVVKQU1qazNZVEEyWTJFMVpUazNObVU0WkRCaE5EazBNakEwXCJ9IiwiYXMiOiJpbXMtbmExLXN0ZzEiLCJhYV9pZCI6IjI0MTAxOTc2NjY0QjE5OUYwQTQ5NDAzMUBjNjJmMjRjYzViNWI3ZTBlMGE0OTQwMDQiLCJjdHAiOjAsImZnIjoiWkJLWVo2UjQ3WjJYQjREWjNHWk1BMklBVTQ9PT09PT0iLCJzaWQiOiIxNzM0NDMwNjk2NzUzX2JiYzI4ZmVhLTdjMjEtNGExMS05NjNkLTc5NTc5ZGEyMTgxMl91dzIiLCJtb2kiOiJmMWZjNGYiLCJwYmEiOiJPUkcsTWVkU2VjTm9FVixMb3dTZWMiLCJleHBpcmVzX2luIjoiMjg4MDAwMDAiLCJzY29wZSI6ImFiLm1hbmFnZSxhY2NvdW50X2NsdXN0ZXIucmVhZCxhZGRpdGlvbmFsX2luZm8sYWRkaXRpb25hbF9pbmZvLmpvYl9mdW5jdGlvbixhZGRpdGlvbmFsX2luZm8ucHJvamVjdGVkUHJvZHVjdENvbnRleHQsYWRkaXRpb25hbF9pbmZvLnJvbGVzLEFkb2JlSUQsYWRvYmVpby5hcHByZWdpc3RyeS5yZWFkLGFkb2JlaW9fYXBpLGF1ZGllbmNlbWFuYWdlcl9hcGksY3JlYXRpdmVfY2xvdWQsbXBzLG9wZW5pZCxvcmcucmVhZCxwcHMucmVhZCxyZWFkX29yZ2FuaXphdGlvbnMscmVhZF9wYyxyZWFkX3BjLmFjcCxyZWFkX3BjLmRtYV90YXJ0YW4sc2VydmljZV9wcmluY2lwYWxzLndyaXRlLHNlc3Npb24iLCJjcmVhdGVkX2F0IjoiMTczNDQzMDgzMDUzMyJ9.WoBOdeffWy5eJPcCMS7TBYw0hO-N7PgXq1HtOlddMgXT8JJ2DxAi_WMZlUpLW12x22caAOGwEKBosuy_ETFHAem3YnUskLLFo6p4lg9qy6WfG7-sqiIAdo5SrCV89HHZUR3qPQIQj8X04VVqUSjeTJKywRrsW3BLJjIakBrTsranYZ8Dh2bBXpHLxmM77a8YRLr9wwAHJ3oqDSiJgG9jNMT2ekMx6BRiUKENg8sXsZLwk0ESEaEMSTIJve6VqWAGMLELikkEfUSEI1gXeO82SCA44GExcNxhBhj0VNQNWzkR6CUKcu-XA7boebPlTwcoEoCeoo5A6kZ7yF4mJTjIHA"
 
-    makeGraphQLRequest(getAllBuyingGroupsV2Query, token, object : ApiCallback {
+    makeGraphQLRequest(queryPayload, token, object : ApiCallback {
         override fun onSuccess(response: String) {
             println("Success: $response")
         }
